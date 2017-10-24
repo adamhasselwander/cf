dc09a2b519078144d94a5a38743c930a6c718f22
Adam Hasselwander <adam.hasselwander@gmail.com>
2017-10-24 13:15:38 +0200

    877 D (untested)

--- 877/877/D/D.cpp
+++ 877/877/D/D.cpp
@@ -22,8 +22,13 @@ typedef vector<int> vi;
 typedef vector<long> vl;
 typedef pair<int, int> pii;
 
-vi hor[1000];
-vi ver[1000];
+struct state {
+	int x, y, depth;
+};
+
+queue<state> q;
+bool grid[1000][1000];
+bool visited[1000][1000];
 
 int n, m, k;
 int x1, y1, x2, y2;
@@ -34,8 +39,39 @@ int bfs(int x, int y, int depth) {
 		exit(0);
 	}
 
-	for(int xx : hor[y])
+	if (visited[x][y]) return;
+	visited[x][y] = 1;
 
+	bool next = true;
+	rep(i, x + 1, min(x + k + 1, m))
+		if (grid[i][y]) {
+			q.push({ i - 1, y, depth + 1 });
+			next = false;
+		}
+	if (next) q.push({ x + k, y, depth + 1 });
+
+	next = true;
+	revrep(i, x - 1, max(x - k - 1, 0))
+		if (grid[i][y]) {
+			q.push({ i + 1, y, depth + 1 });
+		}
+	if (next) q.push({ x - k, y, depth + 1 });
+
+	next = true;
+	rep(i, y + 1, min(y + k + 1, n))
+		if (grid[x][i]) {
+			q.push({ x, i + 1, depth + 1 });
+			next = false;
+		}
+	if (next) q.push({ x, y + k, depth + 1 });
+
+	next = true;
+	revrep(i, y - 1, max(y - k - 1, 0))
+		if (grid[x][i]) {
+			q.push({ x, i + 1, depth + 1 });
+			next = false;
+		}
+	if (next) q.push({ x, y - k, depth + 1 });
 }
 
 int main() {
@@ -47,12 +83,17 @@ int main() {
 			char c;
 			sc(c);
 			if (c == '.') {
-				ver[i].push_back(j);
-				hor[j].push_back(i);
+				grid[j][i] = 1;
 			}
 		}
 	}
 	sd(x1), sd(y1), sd(x2), sd(y2);
 
 	bfs(x1, y1, 0);
+
+	while (!q.empty()) {
+		state s = q.front();
+		q.pop();
+		bfs(s.x, s.y, s.depth);
+	}
 }
