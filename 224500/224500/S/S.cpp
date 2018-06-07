60d9988605f8fd0365a274d98f28ebd20004062c
Adam Hasselwander <adam.hasselwander@gmail.com>
2018-06-07 23:24:40 +0200

    S+R=SR

--- 224500/224500/S/S.cpp
+++ 224500/224500/S/S.cpp
@@ -6,7 +6,7 @@
 #define ss(n) scanf("%s",n) == 0
 #define sc(n) scanf(" %c",&n) == 0
 
-#define pd(x) printf("%d ", x)
+#define pd(x) printf("%d", x)
 #define ps(x) printf("%s", x)
 #define pl(x) printf("%I64d", x)
 
@@ -24,36 +24,14 @@ typedef pair<int, int> pii;
 
 
 int main() {
-	int a, b, c;
-	sd(a), sd(b), sd(c);
+	ll k, n, w;
+	sl(k), sl(n), sl(w);
 
-	//a = e3 + e2, // e3 = bonds 1 - 3, e2 = bonds 1 - 2
-	//b = (c - e3) + e2, // c - e3 = bonds 2 - 3, e2 = bonds 1 - 2
-	//c = (a - e1) + e3, // 
-
-	rep(i, 0, a + 1) {
-
-		// 1. Guess the first one calculate the other two!
-
-		int e13 = i;
-		int e12 = a - i;
-		int e23 = b - e12;
-		
-		if (c == e13 + e23 && e13 >= 0 && e12 >= 0 && e23 >= 0) {
-			pd(e12);
-			pd(e23);
-			pd(e13);
-			return 0;
-		}
-
+	ll b = 0;
+	rep(i, 1, w + 1) { // This is a geometric series and could be calculated like k^n something something
+		b += i * k;
 	}
 
-	ps("Impossible");
-
-	//e1 = a - e2;
-	//e2 = b - e3;
-	//e3 = c - e1;
-
-	//e1 = a - (b - e3);
+	pl(max(b - n, 0LL));
 
 }
